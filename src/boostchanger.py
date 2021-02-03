2a1282c2186e81cbc4234d237b360a21de9b6aaa
nbebaw <nader.bebawy@gmx.de>
2021-02-03 22:16:47 +0100

    fix absolute path problem for icons

--- src/boostchanger.py
+++ src/boostchanger.py
@@ -27,11 +27,14 @@ def turboOff():
     )
 
 
+# Get the absolute path of the directory
+path = os.path.dirname(os.path.abspath(__file__))
 # Add icons
-icon = QIcon("./src/icons/icon.png")
-OnIcon = QIcon("./src/icons/on.png")
-OffIcon = QIcon("./src/icons/off.png")
-exitIcon = QIcon("./src/icons/exit.png")
+icon = QIcon(os.path.join(path, "icons/icon.png"))
+print(os.path.join(path, "icons/icon.png"))
+OnIcon = QIcon(os.path.join(path, "icons/on.png"))
+OffIcon = QIcon(os.path.join(path, "icons/off.png"))
+exitIcon = QIcon(os.path.join(path, "icons/exit.png"))
 
 # Adding item on the menu bar
 tray = QSystemTrayIcon()
